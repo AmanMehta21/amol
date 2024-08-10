4bd8976eee0a2cec054cd9325c8716921fabd6b4
AmanMehta21 <30947277+AmanMehta21@users.noreply.github.com>
2024-08-10 22:30:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "${var.bucket_name}"
   acl    = "private"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro" 
+
+  tags = {
+    Name = "TerraformExample"
+  }
+}
